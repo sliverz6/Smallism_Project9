7f37b9dfbd92d7ae18993b6aaae6195aaf798b9d
sliverz6 <52848569+sliverz6@users.noreply.github.com>
2020-05-19 13:29:33 +0900

    sada

--- project9_05_ToDoList/assets/scripts/app.js
+++ project9_05_ToDoList/assets/scripts/app.js
@@ -1,70 +1,154 @@
-class Calendar {
-    constructor() {
-        this.monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        this.dayOfWeekList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        this.date = new Date();
-        this.month = this.monthList[this.date.getMonth()];
-        this.day = this.date.getDate();
-        this.dayOfWeek = this.dayOfWeekList[this.date.getDay()];
+class TaskLog {
+    constructor(taskList) {
+        this.taskList = taskList;
+
+        this.parsing();
     }
-    
-    render() {
-        const dateEl = document.querySelector('.date');
-        dateEl.innerHTML = `${this.dayOfWeek}, <span>${this.day} ${this.month}<span>`;
-        dateEl.lastElementChild.className = 'r-font-weight';
+
+    updateUI(taskList) {
+        this.taskNumEl.textContent = `0${taskList.length}`;
+    }
+
+    parsing() {
+        this.taskNumEl = document.querySelector('.created-number');
+
+        this.taskNumEl.textContent = `0${this.taskList.length}`;
     }
 }
 
-class TaskLog {}
+class TaskList {
+    taskList = [];
 
-class TaskList {}
+    constructor() {
+        this.renderHook = document.querySelector('.task');
+    }
 
-class Task {}
+    addTask(task) {
+        this.taskList.push(task);
+
+        this.render();
+
+        App.updateTaskUI(this.taskList);
+    }
+
+    deleteTaskHandler(taskEl) {
+        const taskContent = taskEl.querySelector('.content__text').textContent;
+
+        this.renderHook.remove(taskEl);
+    }
+
+    render() {
+        if (this.taskList.length === 0) {
+            return;
+        }
+
+        this.renderHook.innerHTML = ``;
+
+        for (const task of this.taskList) {
+            const taskEl = document.createElement('li');
+            taskEl.className = 'list-items';
+            taskEl.innerHTML = `
+                <button class="content__completed"><i class="fas fa-check content__completed__icon"></i></button>
+                <p class="content__text">${task.content}</p>
+                <button class="remove-task-btn"><i class="fas fa-trash-alt"></i></button>
+            `;
+
+            const deleteTaskBtn = taskEl.querySelector('.remove-task-btn');
+            deleteTaskBtn.addEventListener('click', this.deleteTaskHandler.bind(this, taskEl));
+
+            this.renderHook.append(taskEl);
+        }
+    }
+}
+
+class Task {
+    constructor(content) {
+        this.content = content;
+    }
+}
 
 class AddTaskModal {
     constructor() {
-        this.addTaskModalEl = document.querySelector('.add-task-modal');
-        this.closeModalBtnEl = this.addTaskModalEl.children[1];
-        this.backdropEl = document.getElementById('backdrop');
+        this.renderModal();
+        this.renderBackdrop();
     }
 
-    render() {
-        this.addTaskModalEl.classList.add('visible');
-        this.backdropEl.classList.add('visible');
-
-        this.backdropEl.addEventListener('click', this.closeTaskModal.bind(this));
-        this.closeModalBtnEl.addEventListener('click', this.closeTaskModal.bind(this));
+    closeModal() {
+        const addTaskModalEl = document.querySelector('.add-task-modal');
+        addTaskModalEl.remove();
+        const backdropEl = document.getElementById('backdrop');
+        backdropEl.remove();
     }
 
-    closeTaskModal() {
-        this.backdropEl.classList.remove('visible');
-        this.addTaskModalEl.classList.remove('visible');
+    confirmTaskHandler(taskInput) {
+        const taskInputValue = taskInput.value;
+
+        if (taskInputValue.trim() === '') {
+            alert('할 일을 작성해주세요.');
+            return;
+        }
+
+        const task = new Task(taskInputValue);
+        App.addTaskToList(task);
+
+        this.closeModal();
     }
-}
 
-class AddTaskBtn {
-    constructor() {
-        this.addTastBtnEl = document.querySelector('.add-task-btn');
+    renderBackdrop() {
+        const backdropEl = document.createElement('div');
+        backdropEl.id = 'backdrop';
 
-        this.addTastBtnEl.addEventListener('click', this.addTaskHandler);
+        backdropEl.addEventListener('click', this.closeModal.bind(this));
+
+        document.body.append(backdropEl);
     }
 
-    addTaskHandler() {
-        App.startTaskModal();
+    renderModal() {
+        const addTaskModalEl = document.createElement('section');
+        addTaskModalEl.className = 'add-task-modal';
+        addTaskModalEl.innerHTML = `
+            <input type="text" placeholder="할 일을 작성하세요.">
+            <button class="close-add-modal-btn"><i class="fas fa-arrow-left"></i></button>
+            <button class="confirm-add-modal-btn"><i class="fas fa-plus"></i></button>
+        `;
+        
+        const closeModalBtn = addTaskModalEl.querySelector('.close-add-modal-btn');
+        closeModalBtn.addEventListener('click', this.closeModal.bind(this));
+        
+        const taskInput = addTaskModalEl.querySelector('input');
+        
+        const confrimTaskBtn = addTaskModalEl.querySelector('.confirm-add-modal-btn');
+        confrimTaskBtn.addEventListener('click', this.confirmTaskHandler.bind(this, taskInput));
+        
+        document.body.append(addTaskModalEl);
     }
 }
 
 class App {
-    static startTaskModal () {
-        this.addTaskModal.render();
+    static init() {
+        App.parsing();
+        const taskList = new TaskList('task');
+        this.taskList = taskList;
+        const taskLog = new TaskLog(this.taskList.taskList);
+        this.taskLog = taskLog;
     }
 
-    static init() {
-        this.addTaskModal = new AddTaskModal();
-        this.addTaskBtn = new AddTaskBtn();
-        this.calendar = new Calendar();
+    static updateTaskUI(taskList) {
+        this.taskLog.updateUI(taskList);
+    }
 
-        this.calendar.render();
+    static addTaskToList(task) {
+        this.taskList.addTask(task);
+    }
+
+    static startAddTaskHandler() {
+        new AddTaskModal();
+    }
+
+    static parsing() {
+        const startAddTaskBtn = document.querySelector('.add-task-btn');
+
+        startAddTaskBtn.addEventListener('click', App.startAddTaskHandler);
     }
 }
 
